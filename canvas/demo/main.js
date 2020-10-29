b4dec0dfee7ade99221d45a3d8ff3b40e5b67375
Vincent Letourneau <vincent@nanoninja.com>
2020-10-29 17:34:27 +0100

    Add shapes

---
+++ canvas/demo/main.js
@@ -0,0 +1,24 @@
+// Initialize
+const canvas = document.querySelector('#stage');
+const context = canvas.getContext('2d');
+
+// Parametrer le canvas.
+canvas.width = 800;
+canvas.height = 600;
+canvas.style.backgroundColor = '#ccc';
+
+// Changer la couleur
+context.fillStyle = 'orangered';
+context.strokeStyle = '#000';
+context.lineWidth = 4;
+
+// Dessiner un rectangle.
+context.fillRect(0, 0, 400, 300);
+context.strokeRect(2, 2, 400, 300); // 408 x 308
+context.fillRect(400, 300, 400, 300);
+
+context.fillStyle = 'blue';
+
+// Dessiner un cercle.
+context.arc(400, 300, 100, 0, Math.PI*2, true);
+context.fill();
