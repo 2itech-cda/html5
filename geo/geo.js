4afcf03161ff3345cd335016ccf52d39c8750b77
Vincent Letourneau <vincent@nanoninja.com>
2020-10-29 15:38:17 +0100

    Add controller

---
+++ geo/geo.js
@@ -0,0 +1,9 @@
+const layerDefault = {
+    url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+    attribution: 'Default map'
+}
+
+const layerWorldStreet = {
+    url: 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}',
+    attribution: 'World Street'
+}
