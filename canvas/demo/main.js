64ba5e10e9c86710c38e760e20faf47cc84e718e
Vincent Letourneau <vincent@nanoninja.com>
2020-10-30 09:44:02 +0100

    Add sprites

--- canvas/demo/main.js
+++ canvas/demo/main.js
@@ -8,7 +8,7 @@ canvas.height = 600;
 canvas.style.backgroundColor = '#ccc';
 
 // Changer la couleur
-context.fillStyle = 'orangered';
+context.fillStyle = 'royalblue';
 context.strokeStyle = '#000';
 context.lineWidth = 4;
 
@@ -17,8 +17,10 @@ context.fillRect(0, 0, 400, 300);
 context.strokeRect(2, 2, 400, 300); // 408 x 308
 context.fillRect(400, 300, 400, 300);
 
-context.fillStyle = 'blue';
+context.fillStyle = 'black';
 
 // Dessiner un cercle.
 context.arc(400, 300, 100, 0, Math.PI*2, true);
 context.fill();
+
+context.context.rotate(Math.PI / 180 * degree);
