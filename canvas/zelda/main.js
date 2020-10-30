a5c35e4a94064b23d56205f9a5e41543ae55d826
Vincent Letourneau <vincent@nanoninja.com>
2020-10-30 11:48:04 +0100

    update canvas

--- canvas/zelda/main.js
+++ canvas/zelda/main.js
@@ -3,3 +3,17 @@ const ctx = canvas.getContext('2d');
 
 const imgBg = getImage('img/zelda-bg-01.jpg');
 const imgLink = getImage('img/link.png');
+
+function main() {
+
+    // 1280 x 720
+    ctx.drawImage(
+        imgBg,
+        0, 0, 1280, 720
+    );
+    
+}
+
+main();
+
+// setInterval(main, 1000/24);
