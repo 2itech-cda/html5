7258da56753cec0ab228c221d370be26cf684085
Vincent Letourneau <vincent@nanoninja.com>
2020-11-07 23:49:40 +0100

    improve coin animation

--- canvas/demo/main.js
+++ canvas/demo/main.js
@@ -14,13 +14,13 @@ context.lineWidth = 4;
 
 // Dessiner un rectangle.
 context.fillRect(0, 0, 400, 300);
-context.strokeRect(2, 2, 400, 300);   // 408 x 308
+context.strokeRect(2, 2, 400, 300); // 408 x 308
 context.fillRect(400, 300, 400, 300); // fillRect(x, y, w, h)
 
 context.fillStyle = 'black';
 
 // Dessiner un cercle.
-context.arc(400, 300, 100, 0, Math.PI*2, true);
+context.arc(400, 300, 100, 0, Math.PI * 2, true);
 context.fill();
 
 // Dessier des lignes
@@ -29,13 +29,13 @@ context.fill();
 context.fillStyle = 'white';
 
 context.beginPath();
-context.moveTo(50, 50);   // on position le début de mon tracé
-context.lineTo(200, 50);  // on trace une ligne
+context.moveTo(50, 50); // on position le début de mon tracé
+context.lineTo(200, 50); // on trace une ligne
 context.lineTo(200, 175);
 context.lineTo(50, 175);
-context.closePath();      // on ferme la forme
-context.fill();           // on rempli la forme fermée
-context.stroke();         // on trace les lignes
+context.closePath(); // on ferme la forme
+context.fill(); // on rempli la forme fermée
+context.stroke(); // on trace les lignes
 
 // On dessine une image.
 // https://developer.mozilla.org/fr/docs/Web/API/CanvasRenderingContext2D/drawImage
@@ -50,37 +50,35 @@ link.onload = () => {
     // context.drawImage(image, 0, 0, 450/2, 360/2);
 
     context.drawImage(link, 300, 0, 450, 360);
-
     context.drawImage(
         link,
-        0, 0, 90, 90,  // source (recadrage, rogner, crop)
+        0, 0, 90, 90,    // source (recadrage, rogner, crop)
         50, 30, 150, 150 // destination de la source sur le canvas
     );
 
     // affiche de la première pièce
-    // 1000w x 10h
+    // 1000 width x 10 height
     const coin = new Image();
     coin.src = './img/coins.png';
 
     let x = 0;
-    let speed = 5;
+    const fps = 25;
 
-    coin.onload = () => {
-        setInterval(() => {
-            context.clearRect(200, 400, 100, 100);
+    function main() {
+        context.clearRect(170, 420, 50, 50);
+        context.drawImage(
+            coin,
+            x, 0, 100, 100,
+            170, 420, 50, 50,
+        );
 
-            context.drawImage(
-                coin,
-                x, 0, 100, 100,
-                200, 400, 100, 100,
-            );
+        x += 100;
+        if (x === 1000) {
+            x = 0;
+        }
 
-            x += 100;
-
-            if (x === canvas.width) {
-                x = 0;
-            }
-
-        }, 1000/24 - speed);
+        setTimeout(() => requestAnimationFrame(main), 1000 / fps);
     }
+
+    main();
 };
